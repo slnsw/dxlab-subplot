3430182a062eb8d161aed5605d60e867a242abe7
Carlos Arroyo <carroyo@aam-us.org>
2020-02-14 22:42:20 +1100

    WIP Controlling when images should be load in background or not

--- src/components/layers/TileImagesLayer.js
+++ src/components/layers/TileImagesLayer.js
@@ -1,122 +1,95 @@
 
 import { CompositeLayer } from 'deck.gl';
-import { TileLayer } from '@deck.gl/geo-layers';
-import { BitmapLayer, PolygonLayer } from '@deck.gl/layers';
+import { BitmapLayer, GeoJsonLayer } from '@deck.gl/layers';
+
+import { getImageUrl } from '../../share/utils';
 
 import { load } from "@loaders.gl/core";
 import { max, min } from 'lodash'
 
 
-const tileServer = 'http://dxmap.dimaginarium.com/tiled/'
-
-// [[west, south], [west, north], [east, north], [east, south], [west, south]]
-// const bounds = [[151.214204, -33.864048, 50], [151.210451, -33.864039, 50], [151.210455, -33.859921, 50], [151.214208, -33.859929, 50]]
-const bounds = [[151.214204, -33.864048], [151.210451, -33.864039], [151.210455, -33.859921], [151.214208, -33.859929]]
 
 const areRectanglesOverlap = (a, b) => {
   const visible = !(
-      b.left < a.left ||
-      b.top < a.top ||
-      b.right > a.right ||
-      b.bottom > a.bottom
-    )
+    b.left < a.left ||
+    b.top < a.top ||
+    b.right > a.right ||
+    b.bottom > a.bottom
+  )
 
   return visible
 }
+
+let promise = null
 
 export class TileImagesLayer extends CompositeLayer {
 
 
 
+  updateState({ props, changeFlags }) {
+    if (changeFlags.dataChanged) {
+      const { data, suffix } = props;
+
+      if (!data) {
+        return;
+      }
+
+      const featuresData = data.reduce(function (result, el) {
+        const { geometry, properties } = el;
+        if (geometry) {
+          const elevation = 50;
+          //  const { filter} = this.props;
+          //  const { fromYear, toYear } = filter;
+          //  const elevation = interpolateScale(parseInt(m.year), toYear, fromYear) * 50; 
+          //  mapValue(m.year, this.state.year_from, this.state.year_to, 0, this.state.year_to - this.state.year_from);
+
+          const image = getImageUrl(properties.asset_id, suffix, '80,');
+          const feature = {
+            ...el,
+            geometry: {
+              ...geometry,
+              coordinates: [geometry.coordinates[0].map((c) => {
+                c.push(elevation);
+                return c;
+              })]
+            },
+            properties: {
+              ...properties,
+              elevation,
+              image_url: image,
+              // IMPORTANT: Change image bound structure to a single array
+              // Deck.gl API needs image bounds in a single array. 
+              image_bounds: properties.image_bounds.coordinates[0].map((c) => {
+                c.push(elevation);
+                return c;
+              }),
+
+            }
+          }
+
+          result.push(feature);
+        }
+        return result;
+      }, []);
+
+      const feature = {
+        'type': 'FeatureCollection',
+        'features': featuresData
+
+      }
+      this.setState({ feature });
+
+    }
+  }
 
   shouldUpdateState({ props, oldProps, context, oldContext, changeFlags }) {
-
-    // const visible = areRectanglesOverlap(this.getViewBounds(), this.getImageBounds());
-
-    // this.setState({visible})
-
-    return (changeFlags.viewportChanged !== false );
-  }
-
-  loadTileImages() {
-
-    // 1. Load iif info.json file and extract height and with
-    // Temporal
-    const height = 4493
-    const width = 6495
-    const tileWidth = 256
-    const tileHeight = 256
-    const minZoom = 0
-    const maxZoom = 13
-
-    // const viewport = this.context.viewport
-    // const nw = viewport.unproject([0, 0]);
-    // const se = viewport.unproject([viewport.width, viewport.height]);
-
-    // console.log(nw, se)
-
-    return new TileLayer({
-      pickable: false,
-      opacity: 1,
-      // https://wiki.openstreetmap.org/wiki/Zoom_levels
-      // minZoom: 0,
-      // maxZoom: 13,
-
-      getTileData: ({ x, y, z, ...others }) => {
-        const scale = Math.pow(0.5, maxZoom - z)
-        const levelWidth = Math.ceil(width * scale)
-        const levelHeight = Math.ceil(height * scale)
-        const iiifTileSizeWidth = Math.ceil(tileWidth / scale);
-        const iiifTileSizeHeight = Math.ceil(tileHeight / scale);
-
-        const iiifTileX = 0; // x * iiifTileSizeWidth;
-        const iiifTileY = 0; // y * iiifTileSizeHeight;
-        const iiifTileW = Math.min(iiifTileSizeWidth, width - iiifTileX);
-        const iiifTileH = Math.min(iiifTileSizeHeight, height - iiifTileY);
-
-        // console.log(this.context)
-
-        // console.log(x,y,z);
-        // console.count(z)
-        // return Promise.resolve(null);
-
-        // console.log(scale, levelWidth, levelHeight, iiifTileSizeWidth, iiifTileSizeHeight, iiifTileW, iiifTileH)
-
-        // return load(`${tileServer}/a1367540.tif/${iiifTileX},${iiifTileY},${iiifTileW},${iiifTileH}/full/0/default.jpg`)
-        // console.log(others);
-        // [[west, south], [west, north], [east, north], [east, south], [west, south]]
-        const imageWest = bounds[0][0]
-        const imageNorth = bounds[1][1]
-        const imageSouth = bounds[0][1]
-        const imageEast = bounds[2][0]
-
-        const { bbox: { west, north, east, south } } = others
-        // console.log(areRectanglesOverlap([west, north, east, south], [imageWest, imageNorth, imageEast, imageSouth] ));
-
-
-        return load(`${tileServer}/a1367540.tif/${iiifTileX},${iiifTileY},${iiifTileW},${iiifTileH}/full/0/default.jpg`)
-        // return Promise.resolve(null);
-      },
-
-      renderSubLayers: props => {
-        const {
-          bbox: { west, south, east, north }
-        } = props.tile;
-
-        // return null;
-        return new BitmapLayer(props, {
-          opacity: .5,
-          data: [],
-          image: props.data,
-          bounds: bounds
-        });
-      }
-    })
+    return (changeFlags.viewportChanged !== false);
   }
 
 
-  getImageBounds() {
-    const points = bounds.map( p => this.context.viewport.project(p))
+
+  getImageBounds(bounds) {
+    const points = bounds.map(p => this.context.viewport.project(p))
     const longs = points.map((c) => c[0])
     const lats = points.map((c) => c[1])
 
@@ -126,16 +99,23 @@ export class TileImagesLayer extends CompositeLayer {
     const right = min(longs)
     const top = max(lats)
     const bottom = min(lats)
-    return {top, right, bottom, left}
+    return { top, right, bottom, left }
 
   }
 
   getViewBounds() {
     const viewport = this.context.viewport
-    return {top: 100, right: viewport.width -100, bottom: viewport.height-100, left: 100}
+    return { top: 100, right: viewport.width - 100, bottom: viewport.height - 100, left: 100 }
   }
 
   loadImages() {
+    const tileServer = 'http://dxmap.dimaginarium.com/tiled/'
+
+    // [[west, south], [west, north], [east, north], [east, south], [west, south]]
+    // const bounds = [[151.214204, -33.864048, 50], [151.210451, -33.864039, 50], [151.210455, -33.859921, 50], [151.214208, -33.859929, 50]]
+    const bounds = [[151.214204, -33.864048], [151.210451, -33.864039], [151.210455, -33.859921], [151.214208, -33.859929]]
+
+
     const z = this.context.viewport.zoom
     const height = 4493
     const width = 6495
@@ -144,67 +124,144 @@ export class TileImagesLayer extends CompositeLayer {
     const minZoom = 0
     const maxZoom = 13
 
-    const scale = Math.pow(0.5, maxZoom - z)
-    const levelWidth = Math.ceil(width * scale)
-    const levelHeight = Math.ceil(height * scale)
+    const scale = Math.ceil(Math.pow(0.5, maxZoom - z))
+
     const iiifTileSizeWidth = Math.ceil(tileWidth / scale);
     const iiifTileSizeHeight = Math.ceil(tileHeight / scale);
 
-    const iiifTileX = 0; // x * iiifTileSizeWidth;
-    const iiifTileY = 0; // y * iiifTileSizeHeight;
-    const iiifTileW = Math.min(iiifTileSizeWidth, width - iiifTileX);
-    const iiifTileH = Math.min(iiifTileSizeHeight, height - iiifTileY);
 
+    // 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096
+    // 
+    const image = `${tileServer}a1367540.tif/full/4096,/0/default.png`
+    const inViewport = areRectanglesOverlap(this.getViewBounds(), this.getImageBounds(bounds));
 
-    // console.log(z)
-    // [[west, south], [west, north], [east, north], [east, south], [west, south]]
-    // const viewport = this.context.viewport
-    // const nw = viewport.unproject([100, 100]);
-    // const ne = viewport.unproject([viewport.height - 100, 100]);
-    // const se = viewport.unproject([viewport.width - 100, viewport.height - 100]);
-    // const sw = viewport.unproject([100, viewport.height - 100]);
+    // let cancel = null
+    // promise = new Promise((resolve, reject) => {
+    //   cancel = reject;
+    //   if (inViewport) {
+    //     console.log('visible')
+    //     setTimeout(() => {
+    //       load(image).then((r) => {
+    //         console.log('loaded', r.src);
+    //         resolve(r)
+    //       }).catch((err) => {
+    //         // console.log(err);
+    //         reject(err)
+    //       })
+    //     }
+    //       , 5000);
+    //     // resolve()
+    //   } else {
+    //     console.log('hidden')
+    //     resolve(null)
+    //   }
+    // })
 
+    // console.log(cancel);
+    // setTimeout(() => {
+    //   console.log('cancel'); 
+    //   promise.catch(()=> {})
+    //   cancel()
+    // }, 2000)
 
-    // const boundsView = [
-    //   [nw[0], nw[1]],
-    //   [ne[0], ne[1]], 
-    //   [se[0], se[1]], 
-    //   [sw[0], sw[1]],
-    //   [nw[0], nw[1]]
-    // ]
-    // console.log(boundsView)
+    // console.log(promise)
 
-    // const image = `${tileServer}/a1367540.tif/full/200,/0/default.jpg`;
-
-    const visible = areRectanglesOverlap(this.getViewBounds(), this.getImageBounds());
-
-    const image = load(`${tileServer}/a1367540.tif/full/${iiifTileSizeWidth},/0/default.jpg`)
     return [
-        ( visible &&[new BitmapLayer({
-          id: `lod-image`,
-          bounds: bounds,
-          opacity: 1,
-          pickable: false,
-          autoHighlight: false,
-          image: image
-        })]),
-
-        // new PolygonLayer({
-        //   id: `lod-image-bounds`,
-        //   opacity: .2,
-        //   data: [boundsView],
-        //   getPolygon: d => d,
-        // })
+      (inViewport && [new BitmapLayer({
+        id: `lod-image`,
+        bounds: bounds,
+        opacity: 1,
+        pickable: false,
+        autoHighlight: false,
+        image: load(image)
+      })]),
     ];
   }
 
 
+  loadImage(url, inViewport) {
+    let cancel = null
+    return new Promise((resolve, reject) => {
+      cancel = reject;
+      if (inViewport) {
+        console.log('visible')
+        setTimeout(() => {
+          load(url).then((r) => {
+            console.log('loaded', r.src);
+            resolve(r)
+          }).catch((err) => {
+            // console.log(err);
+            reject(err)
+          })
+        }
+          , 5000);
+        // resolve()
+      } else {
+        console.log('hidden')
+        resolve(null)
+      }
+    })
+
+  }
+
+  buildLayers() {
+    const { id, name } = this.props;
+    const { feature: { features } } = this.state;
+    const layers = [];
+
+    // layers.push(features.map(({ properties: { asset_id, image_bounds, image_url } }) => {
+
+    //   return new BitmapLayer(this.getSubLayerProps({
+    //     id: `${id}-bitmap-layer-${name}-${asset_id}`,
+    //     bounds: image_bounds,
+    //     opacity: 0.8,
+    //     pickable: false,
+    //     autoHighlight: false,
+    //     image: image_url
+    //   }));
+
+    // }));
+
+    const viewportRect = this.getViewBounds()
+    const visible = features.filter(({ properties: { image_bounds } }) => areRectanglesOverlap(viewportRect, this.getImageBounds(image_bounds)))
+
+    layers.push(visible.map(({ properties: { asset_id, image_bounds, image_url } }) => {
+      console.log(image_url)
+
+
+      return new BitmapLayer(this.getSubLayerProps({
+        id: `${id}-bitmap-layer-${name}-${asset_id}`,
+        bounds: image_bounds,
+        opacity: 0.8,
+        pickable: false,
+        autoHighlight: false,
+        image: image_url
+      }));
+
+    }));
+
+
+    layers.push(new GeoJsonLayer(this.getSubLayerProps({
+      id: `${id}-bitmap-layer-${name}-cutlines`,
+      data: this.state.feature,
+      pickable: true,
+      autoHighlight: true,
+      stroked: true,
+      getFillColor: [0, 0, 0, 0],
+      getLineColor: [0, 0, 0, 125],
+
+    })));
+
+    return layers;
+
+  }
+
 
   renderLayers() {
-    // return this.loadTileImages();
+    // return this.buildLayers();
     return this.loadImages();
   }
 
 }
 
-TileImagesLayer.layerName = 'SearchResultLayer';
+TileImagesLayer.layerName = 'TileImageLayer';
