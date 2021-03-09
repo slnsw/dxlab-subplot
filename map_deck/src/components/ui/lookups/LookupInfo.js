7bed69bc45dd3b5151ce6fdbe95af7540965ede6
Carlos Arroyo <carroyo@aam-us.org>
2021-03-10 10:33:41 +1100

    perf(map_deck): reduce processing data in result search component

--- map_deck/src/components/ui/lookups/LookupInfo.js
+++ map_deck/src/components/ui/lookups/LookupInfo.js
@@ -1,15 +1,17 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { MapDataContext } from '../../../context/MapsContext'
-import { get, isEmpty } from 'lodash'
+import { get, isEmpty, map } from 'lodash'
 import styles from './LookupInfo.module.scss'
 
 export const LookupInfo = (props) => {
+  const [state, setState] = useState({
+    filteredInfo: {},
+    allInfo: {},
+    placeNameShort: '',
+    radius: ''
+  })
+  const [show, setShow] = useState(false)
   const [mapState] = useContext(MapDataContext)
-
-  const { near = {} } = mapState
-  const show = !isEmpty(near)
-  const { filtered = {}, all = {}, radius, placeName = '' } = near
-  const placeNameShort = placeName.split(',').slice(0, 1).join(',')
 
   const getData = (source) => {
     const data = get(source, 'data', [])
@@ -21,9 +23,21 @@ export const LookupInfo = (props) => {
     }
   }
 
-  // Filtered data
-  const filteredInfo = getData(filtered)
-  const allInfo = getData(all)
+  useEffect(() => {
+    const { near = {} } = mapState
+    const { filtered = {}, all = {}, radius, placeName = '' } = near
+    const placeNameShort = placeName.split(',').slice(0, 1).join(',')
+
+    // Filtered data
+    const filteredInfo = getData(filtered)
+    const allInfo = getData(all)
+
+    setShow(!isEmpty(near))
+    setState({ filteredInfo, allInfo, placeNameShort, radius })
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [mapState.near])
+
+  const { filteredInfo, allInfo, placeNameShort, radius } = state
 
   return (
     <>
